77be5a8a4b5478ddc2aabe41cb2552227bf58815
Jacob Bergfalk <jacobekl@student.chalmers.se>
2025-02-17 13:36:50 +0100

    initial Co-Authored-By Erik Ekström <errkan10@gmail.com> Co-Authored-By Hugo Liss-Daniels <hugolissd@gmail.com> Co-Authored-By Tobias Adrian <tobiasad@gmail.com>

--- Server/src/router/start.ts
+++ Server/src/router/start.ts
@@ -1,9 +1,10 @@
+import cors from "cors";
 import express from "express";
-import { router as gameRouter} from "./gameRouter";
+import { router as gameRouter } from "./gameRouter";
 
 export const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 app.use("/game", gameRouter);
