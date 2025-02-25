a3395d9ce51dbb4df2453592c7a8d53166d52b70
Jacob Bergfalk <jacobekl@student.chalmers.se>
2025-02-25 14:43:09 +0100

    added bannerad Co-Authored-By Erik Ekström <errkan10@gmail.com> Co-Authored-By Hugo Liss-Daniels <hugolissd@gmail.com> Co-Authored-By Tobias Adrian <tobiasad@gmail.com>

--- Client/src/components/Balance.tsx
+++ Client/src/components/Balance.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [data, setData] = useState<any>(null);
@@ -7,19 +7,20 @@ const App = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:8080/game/balance'); //Skickar get till localhosten
-      setData(response.data); 
+      const response = await axios.get("http://localhost:8080/game/balance"); //Skickar get till localhosten
+      const balance = response.data.balance;
+      setData(balance);
     } catch (err) {
-      setError('An error occurred while fetching data'); 
+      setError("An error occurred while fetching data");
       console.error(err);
     }
   };
-  //Använde en knapp bara för att testa här. 
+  //Använde en knapp bara för att testa här.
   return (
     <div>
-      <button onClick={fetchData}>Fetch Data</button> 
+      <button onClick={fetchData}>Fetch Data</button>
       {error && <p>{error}</p>}
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {data && <pre>{"balance: " + data}</pre>}
     </div>
   );
 };
--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,25 +1,39 @@
 import React from "react";
 import "../assets/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="wd-flex justify-content-flex-start">
-      <ul>
-        <li className="navbar_links">
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li className="navbar_links">
-          <Link to="/casino">Casino Slots</Link>
-        </li>
-        <li className="navbar_links">
-          <Link to="/coinflip">Coin Flip</Link>
-        </li>
-        <li className="navbar_links">
-          <Link to="/game">Game</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="wd-flex justify-content-flex-start">
+        <ul>
+          <li className="navbar_links">
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li className="navbar_links">
+            <Link to="/casino">Casino Slots</Link>
+          </li>
+          <li className="navbar_links">
+            <Link to="/coinflip">Coin Flip</Link>
+          </li>
+          <li className="navbar_links">
+            <Link to="/game">Game</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="row">
+        <div className="col-12 d-flex align-items-center justify-content-center">
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+            <img
+              className="header-images"
+              src="images/raid-header.png"
+              alt="banner-ad"
+            />
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
